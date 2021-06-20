dee9358edc4c32e131b892cf221a2577659e6e86
Ie Howe <iehowe_811@hotmail.com>
2021-06-21 01:07:20 +0800

    added dashboards

---
+++ DemoUmbraco/App_Plugins/LWDCustom/dashboards/custom-dashboard.controller.js
@@ -0,0 +1,34 @@
+﻿(function () {
+
+    angular.module('umbraco')
+        .controller('lwd.customDashboardController', LWDCustomDashboardController)
+
+    function LWDCustomDashboardController () {
+        this.title = "Custom Dashboard"
+
+        this.items = [
+            {
+                "icon": "icon-document",
+                "name": "My node 1",
+                "published": true,
+                "description": "A short description of my node",
+                "author": "Author 1"
+            },
+            {
+                "icon": "icon-document",
+                "name": "My node 2",
+                "published": true,
+                "description": "A short description of my node",
+                "author": "Author 2"
+            }
+        ];
+
+        this.options = {
+            includeProperties: [
+                { alias: "description", header: "Description" },
+                { alias: "author", header: "Author" }
+            ]
+        };
+    }
+
+})();
