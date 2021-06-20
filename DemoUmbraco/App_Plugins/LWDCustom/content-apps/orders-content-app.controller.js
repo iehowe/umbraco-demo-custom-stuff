9b6f7fa399264f4155b2a12e5135173407c8314a
Ie Howe <iehowe_811@hotmail.com>
2021-06-21 01:07:28 +0800

    added content apps

---
+++ DemoUmbraco/App_Plugins/LWDCustom/content-apps/orders-content-app.controller.js
@@ -0,0 +1,35 @@
+﻿(function () {
+
+    angular.module('umbraco')
+        .controller('lwd.ordersContentApp', LWDCustomDashboardController)
+
+    function LWDCustomDashboardController() {
+        this.title = "Custom Dashboard"
+
+        this.items = [
+            {
+                "icon": "icon-tag",
+                "name": "Watch 1",
+                "amount": "RM 100",
+                "customer": "Faridah",
+                "date": "June 21, 2021"
+            },
+            {
+                "icon": "icon-tag",
+                "name": "Watch 1",
+                "amount": "RM 100",
+                "customer": "Lim",
+                "date": "June 21, 2021"
+            }
+        ];
+
+        this.options = {
+            includeProperties: [
+                { alias: "amount", header: "Amount" },
+                { alias: "customer", header: "Customer" },
+                { alias: "date", header: "Date" },
+            ]
+        };
+    }
+
+})();
